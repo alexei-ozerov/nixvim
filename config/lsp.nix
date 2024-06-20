07dedd99ae33c634da02df81b2f43955344af727
Alexei Ozerov <aozerov.dev@gmail.com>
2024-06-20 19:06:36 -0400

    Remove broken config, add ts

--- config/lsp.nix
+++ config/lsp.nix
@@ -5,38 +5,16 @@
     servers = {
       rust-analyzer.enable = true;
     };
+    inlayHints = true;
   };
 
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
-    sources = [
-      {name = "nvim_lsp";}
-      {name = "path";}
-      {name = "buffer";}
-      {name = "luasnip";}
-    ];
+  };
 
-    mapping = {
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<Tab>" = {
-        action = ''
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expandable() then
-              luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
-            else
-              fallback()
-            end
-          end
-        '';
-        modes = [ "i" "s" ];
-      };
-    };
+  plugins.treesitter = {
+    enable = true;
+    indent = true;
   };
 }
